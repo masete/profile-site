0cc84c5b7bf5b8ee57d3d48c4cdddab021b0e867
masete <nicholasmasete72@gmail.com>
2022-08-17 01:01:59 +0300

    feat: added the header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,16 @@ import NavBar from './NavBar'
 
 const Header = () => {
   return (
-    <div className='w-full h-full'>
+    <div className='flex w-full h-full'>
       <img className='hidden sm:block absolute w-full h-[750px] object-cover' 
       src='https://cdn.pixabay.com/photo/2016/11/23/14/45/coding-1853305__340.jpg'
           alt='/' />
          <NavBar />
+         <div className='absolute top-[50%] flex flex-wrap justify-center'>
+            <div className='text-white text-3xl pl-[500px]'>JAVASCRIPT</div>
+            <div className='text-white text-3xl pl-[30px]'>PYTHON</div>
+         </div>
+         
     </div>
  
   )
