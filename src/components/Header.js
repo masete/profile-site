7821eec1e8088df9e836a7c7b191e29372086c3d
masete <nicholasmasete72@gmail.com>
2022-08-17 00:35:04 +0300

    feat: added the header body

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='w-full h-full'>
+      <img className='hidden sm:block absolute w-full h-[750px] object-cover' 
+      src='https://cdn.pixabay.com/photo/2016/11/23/14/45/coding-1853305__340.jpg'
+          alt='/' />
+         <NavBar />
+    </div>
+ 
   )
 }
 
