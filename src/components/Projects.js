471a5298a85f116713f0361b09dd95b60146f78d
masete <nicholasmasete72@gmail.com>
2022-08-20 23:35:52 +0300

    feat: worked on

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <>
+        <div className='absolute top-[80%]'>
+          <div>
+            <h1 className='text-3xl mx-auto text-bold'>PROJECTS I HAVE BEEN PART OFF</h1>
+          </div>
+        </div>
+        
+    </>
+ 
   )
 }
 
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import Header from '../components/Header'
+import React from 'react';
+import Header from '../components/Header';
+import Projects from '../components/Projects';
 
 const LandingPage = () => {
   return (
     <>
     <div className=''>
         <Header />
+        <Projects/>
     </div>
     </>
   )
