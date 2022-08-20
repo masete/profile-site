b1e4097534daecf55b9e0d1bee1a9b9850f8d762
masete <nicholasmasete72@gmail.com>
2022-08-20 23:44:45 +0300

    feat: worked on projects feat

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,8 +4,8 @@ const Projects = () => {
   return (
     <>
         <div className='absolute top-[80%]'>
-          <div>
-            <h1 className='text-3xl mx-auto text-bold'>PROJECTS I HAVE BEEN PART OFF</h1>
+          <div className='content-center'>
+            <h1 className='text-3xl text-justify text-bold'>PROJECTS I HAVE BEEN PART OFF</h1>
           </div>
         </div>
         
